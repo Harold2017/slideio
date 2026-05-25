c2f30deaa1bcf5dc6cb303e717e7e1e8d60f7ae5
Booritas <stanislav.melnikov@gmail.com>
2026-05-25 14:30:08 +0200

    Add script for running the tests

--- run-tests-linux.sh
+++
@@ -1,8 +0,0 @@
-./build/release/bin/slideio_converter_tests
-./build/release/bin/slideio_ndpi_tests
-./build/release/bin/slideio_ometiff_tests
-./build/release/bin/slideio_pke_tests
-./build/release/bin/slideio_tests
-./build/release/bin/slideio_testtools
-./build/release/bin/slideio_transformer_tests
-./build/release/bin/slideio_vsi_tests
